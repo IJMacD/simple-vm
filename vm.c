0419c6d6d8ef76d4833a1ad2f0577f38247fde62
Iain MacDonald <IJMacD@gmail.com>
2017-08-27 17:58:10 +0800

    Display more registers

--- vm.c
+++ vm.c
@@ -3,12 +3,20 @@
 
 #include "ben.h"
 
-#define REGISTER_A_X  0
-#define REGISTER_A_Y  0
-#define REGISTER_B_X  0
-#define REGISTER_B_Y  3
-#define ALU_X  0
-#define ALU_Y  6
+#define REGISTER_A_X  55
+#define REGISTER_A_Y   4
+#define REGISTER_B_X  55
+#define REGISTER_B_Y  10
+#define ALU_X         57
+#define ALU_Y          7
+#define PROGRAM_COUNTER_X         55
+#define PROGRAM_COUNTER_Y          1
+#define BUS_X         33
+#define BUS_Y          0
+#define CLOCK_X        2
+#define CLOCK_Y        1
+#define RAM_X          2
+#define RAM_Y          4
 
 #define PRINTF_BINARY_PATTERN_INT4 "%c%c%c%c"
 #define PRINTF_BYTE_TO_BINARY_INT4(i)    \
@@ -42,7 +50,16 @@ unsigned char bus;
 void *hConsole;
 COORD pos;
 
-void printRegister(int x, int y, unsigned char reg) {
+void printRegister4(int x, int y, unsigned char reg) {
+  char output[255];
+  DWORD len = sprintf(output, "0x%02X " PRINTF_BINARY_PATTERN_INT4, reg & 0x0F, PRINTF_BYTE_TO_BINARY_INT4(reg));
+  DWORD dwBytesWritten = 0;
+  pos.X = x;
+  pos.Y = y;
+  WriteConsoleOutputCharacter(hConsole, output, len, pos, &dwBytesWritten);
+}
+
+void printRegister8(int x, int y, unsigned char reg) {
   char output[255];
   DWORD len = sprintf(output, "0x%02X " PRINTF_BINARY_PATTERN_INT8, reg, PRINTF_BYTE_TO_BINARY_INT8(reg));
   DWORD dwBytesWritten = 0;
@@ -60,61 +77,119 @@ void printLabels() {
   pos.X = REGISTER_A_X;
   pos.Y = REGISTER_A_Y;
   const char rega_lbl[] = "Register A";
-  WriteConsoleOutputCharacter(hConsole, rega_lbl, sizeof(rega_lbl), pos, &dwBytesWritten);
+  WriteConsoleOutputCharacter(hConsole, rega_lbl, sizeof(rega_lbl) - 1, pos, &dwBytesWritten);
 
   // RegisterB
   pos.X = REGISTER_B_X;
   pos.Y = REGISTER_B_Y;
   const char regb_lbl[] = "Register B";
-  WriteConsoleOutputCharacter(hConsole, regb_lbl, sizeof(regb_lbl), pos, &dwBytesWritten);
+  WriteConsoleOutputCharacter(hConsole, regb_lbl, sizeof(regb_lbl) - 1, pos, &dwBytesWritten);
 
   // ALU
   pos.X = ALU_X;
   pos.Y = ALU_Y;
   const char alu_lbl[] = "ALU";
-  WriteConsoleOutputCharacter(hConsole, alu_lbl, sizeof(alu_lbl), pos, &dwBytesWritten);
+  WriteConsoleOutputCharacter(hConsole, alu_lbl, sizeof(alu_lbl) - 1, pos, &dwBytesWritten);
+
+  // Program Counter
+  pos.X = PROGRAM_COUNTER_X;
+  pos.Y = PROGRAM_COUNTER_Y;
+  const char counter_lbl[] = "Counter";
+  WriteConsoleOutputCharacter(hConsole, counter_lbl, sizeof(counter_lbl) - 1, pos, &dwBytesWritten);
+
+  // Bus Indicator
+  pos.X = BUS_X;
+  pos.Y = BUS_Y;
+  const char bus_lbl[] = "Bus";
+  WriteConsoleOutputCharacter(hConsole, bus_lbl, sizeof(bus_lbl) - 1, pos, &dwBytesWritten);
+
+  // Clock
+  pos.X = CLOCK_X;
+  pos.Y = CLOCK_Y;
+  const char clock_lbl[] = "Clock";
+  WriteConsoleOutputCharacter(hConsole, clock_lbl, sizeof(clock_lbl) - 1, pos, &dwBytesWritten);
+
+  // RAM
+  pos.X = RAM_X;
+  pos.Y = RAM_Y;
+  const char ram_lbl[] = "RAM";
+  WriteConsoleOutputCharacter(hConsole, ram_lbl, sizeof(ram_lbl) - 1, pos, &dwBytesWritten);
+  pos.X = RAM_X;
+  pos.Y = RAM_Y + 1;
+  const char rama_lbl[] = "Address";
+  WriteConsoleOutputCharacter(hConsole, rama_lbl, sizeof(rama_lbl) - 1, pos, &dwBytesWritten);
+  pos.X = RAM_X;
+  pos.Y = RAM_Y + 2;
+  const char ramc_lbl[] = "Contents";
+  WriteConsoleOutputCharacter(hConsole, ramc_lbl, sizeof(ramc_lbl) - 1, pos, &dwBytesWritten);
 }
 
 void printRegisterA() {
-  printRegister(REGISTER_A_X, REGISTER_A_Y + 1, register_A);
+  printRegister8(REGISTER_A_X, REGISTER_A_Y + 1, register_A);
 }
 
 void printRegisterB() {
-  printRegister(REGISTER_B_X, REGISTER_B_Y + 1, register_B);
+  printRegister8(REGISTER_B_X, REGISTER_B_Y + 1, register_B);
 }
 
 void printALU() {
-  printRegister(ALU_X, ALU_Y + 1, alu);
+  printRegister8(ALU_X, ALU_Y + 1, alu);
+}
+
+void printProgramCounter() {
+  printRegister4(PROGRAM_COUNTER_X, PROGRAM_COUNTER_Y + 1, program_counter);
+}
+
+void printBus() {
+  printRegister8(BUS_X, BUS_Y + 1, bus);
+}
+
+void printRam() {
+  printRegister4(RAM_X + 9, RAM_Y + 1, memory_address);
+  printRegister8(RAM_X + 9, RAM_Y + 2, RAM[memory_address & 0x0F]);
 }
 
 void updateALU() {
   alu = register_A + register_B;
 }
 
+void updateDisplay() {
+  printRegisterA();
+  printRegisterB();
+  printALU();
+  printProgramCounter();
+  printBus();
+  printRam();
+}
+
 void main() {
+  // Create blank console
   hConsole =  CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
   SetConsoleActiveScreenBuffer(hConsole);
-  COORD new_pos = {79, 0};
-  SetConsoleCursorPosition(hConsole, new_pos);
-  CONSOLE_CURSOR_INFO info;
-  GetConsoleCursorInfo(hConsole, &info);
-  // info.dwSize = 25;
-  info.bVisible = info.bVisible ? FALSE : TRUE;
-  SetConsoleCursorInfo(hConsole, &info);
+
+  // Move Cursor out of the way and hide
+  {
+    COORD new_pos = {79, 0};
+    SetConsoleCursorPosition(hConsole, new_pos);
+    CONSOLE_CURSOR_INFO info;
+    GetConsoleCursorInfo(hConsole, &info);
+    // info.dwSize = 25;
+    info.bVisible = info.bVisible ? FALSE : TRUE;
+    SetConsoleCursorInfo(hConsole, &info);
+  }
 
   printLabels();
 
   register_B = 255;
 
-  int i = 255;
-  while(i--){
-    printRegisterA();
-    printRegisterB();
-    printALU();
+  while(1){
+    updateDisplay();
 
+    program_counter++;
     register_A++;
     register_B--;
     updateALU();
+    memory_address++;
 
     Sleep(100);
   }
