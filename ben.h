3c4da1e2585367762f8b180d9afe59b93a0be594
Iain MacDonald <IJMacD@gmail.com>
2017-08-27 22:31:42 +0800

    Working computer

--- ben.h
+++ ben.h
@@ -1,6 +1,6 @@
 #include <inttypes.h>
 
-#define HLT 0b1000000000000000  // Halt clock
+#define HT  0b1000000000000000  // Halt clock
 #define MI  0b0100000000000000  // Memory address register in
 #define RI  0b0010000000000000  // RAM data in
 #define RO  0b0001000000000000  // RAM data out
@@ -14,16 +14,16 @@
 #define OI  0b0000000000010000  // Output register in
 #define CE  0b0000000000001000  // Program counter enable
 #define CO  0b0000000000000100  // Program counter out
-#define J   0b0000000000000010  // Jump (program counter in)
+#define JP  0b0000000000000010  // Jump (program counter in)
 
-uint16_t data[] = {
+uint16_t u_instructions[] = {
   MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 0000 - NOP
   MI|CO,  RO|II|CE,  IO|MI,  RO|AI,  0,         0, 0, 0,   // 0001 - LDA
   MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI,     0, 0, 0,   // 0010 - ADD
   MI|CO,  RO|II|CE,  IO|MI,  RO|BI,  EO|AI|SU,  0, 0, 0,   // 0011 - SUB
   MI|CO,  RO|II|CE,  IO|MI,  AO|RI,  0,         0, 0, 0,   // 0100 - STA
   MI|CO,  RO|II|CE,  IO|AI,  0,      0,         0, 0, 0,   // 0101 - LDI
-  MI|CO,  RO|II|CE,  IO|J,   0,      0,         0, 0, 0,   // 0110 - JMP
+  MI|CO,  RO|II|CE,  IO|JP,  0,      0,         0, 0, 0,   // 0110 - JMP
   MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 0111
   MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1000
   MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1001
@@ -32,16 +32,16 @@ uint16_t data[] = {
   MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1100
   MI|CO,  RO|II|CE,  0,      0,      0,         0, 0, 0,   // 1101
   MI|CO,  RO|II|CE,  AO|OI,  0,      0,         0, 0, 0,   // 1110 - OUT
-  MI|CO,  RO|II|CE,  HLT,    0,      0,         0, 0, 0,   // 1111 - HLT
+  MI|CO,  RO|II|CE,  HT,     0,      0,         0, 0, 0,   // 1111 - HLT
 };
 
-#define I_NOP 0b0000
-#define I_LDA 0b0001
-#define I_ADD 0b0010
-#define I_SUB 0b0011
-#define I_STA 0b0100
-#define I_LDI 0b0101
-#define I_JMP 0b0110
+#define NOP 0b00000000
+#define LDA 0b00010000
+#define ADD 0b00100000
+#define SUB 0b00110000
+#define STA 0b01000000
+#define LDI 0b01010000
+#define JMP 0b01100000
 // 0b0111
 // 0b1000
 // 0b1001
@@ -49,5 +49,5 @@ uint16_t data[] = {
 // 0b1011
 // 0b1100
 // 0b1101
-#define I_OUT 0b1110
-#define I_HLT 0b1111
+#define OPT 0b11100000  // OUT but windows predefines OUT macro
+#define HLT 0b11110000
--- vm.c
+++ vm.c
@@ -27,6 +27,7 @@
 #define CONTROL_Y     18
 
 #define PHASE_COUNT 5
+#define CONTROL_LINES 15
 
 #define PRINTF_BINARY_PATTERN_INT4 "%c%c%c%c"
 #define PRINTF_BYTE_TO_BINARY_INT4(i)    \
@@ -51,11 +52,30 @@ unsigned char register_B = 0x00;
 unsigned char register_I = 0x00;         // Instruction register
 unsigned char register_O = 0x00;         // Output register
 unsigned char program_counter = 0x00;
-unsigned char memory_address = 0x0;
+unsigned char memory_address = 0x00;
 unsigned char alu = 0x00;
-unsigned char RAM[16] = { 0 };
+unsigned char RAM[16] = {
+  LDA | 0x0E,
+  ADD | 0x0F,
+  OPT,
+  HLT,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  28,
+  14
+};
 unsigned char decoder_phase = 0x00;
 unsigned char bus;
+unsigned short decoder_output = 0x00;
+unsigned char halt = 0x00;
 
 void *hConsole;
 COORD pos;
@@ -193,11 +213,17 @@ void printRam() {
 
 void printOutput() {
   printRegister8(OUTPUT_X, OUTPUT_Y + 1, register_O);
+  char output[255];
+  DWORD len = sprintf(output, "%8d", register_O);
+  DWORD dwBytesWritten = 0;
+  pos.X = OUTPUT_X + 5;
+  pos.Y = OUTPUT_Y + 2;
+  WriteConsoleOutputCharacter(hConsole, output, len, pos, &dwBytesWritten);
 }
 
 void printInstruction() {
-  printRegister4(INSTRUCTION_X, INSTRUCTION_Y + 1, register_I);
-  printRegister4(INSTRUCTION_X + 10, INSTRUCTION_Y + 1, register_I >> 4);
+  printRegister4(INSTRUCTION_X, INSTRUCTION_Y + 1, register_I >> 4);
+  printRegister4(INSTRUCTION_X + 10, INSTRUCTION_Y + 1, register_I);
 }
 
 void printDecoder() {
@@ -208,8 +234,8 @@ void printDecoder() {
   char output[PHASE_COUNT * 2];
   int i, offset = 0;
   for(i = 0; i < PHASE_COUNT + 1; i++) {
-    output[i*2  ] = phase == i ? '^' : ' ';
-    output[i*2+1] = ' ';
+    output[offset++] = phase == i ? '^' : ' ';
+    output[offset++] = ' ';
   }
 
   DWORD dwBytesWritten;
@@ -219,13 +245,40 @@ void printDecoder() {
 void printControl() {
   pos.X = CONTROL_X;
   pos.Y = CONTROL_Y + 2;
-  const char control_ind[] = "                                    ——";
+
+  char output[CONTROL_LINES * 3];
+  int i, offset = 0;
+  for(i = 0; i < CONTROL_LINES; i++) {
+    unsigned short bit = 1 << (15 - i);
+    output[offset++] = decoder_output & bit ? '—' : ' ';
+    output[offset++] = decoder_output & bit ? '—' : ' ';
+    output[offset++] = ' ';
+  }
+
   DWORD dwBytesWritten;
-  WriteConsoleOutputCharacter(hConsole, control_ind, sizeof(control_ind) - 1, pos, &dwBytesWritten);
+  WriteConsoleOutputCharacter(hConsole, output, CONTROL_LINES * 3, pos, &dwBytesWritten);
 }
 
-void updateALU() {
-  alu = register_A + register_B;
+printClock() {
+  pos.X = CLOCK_X;
+  pos.Y = CLOCK_Y + 1;
+  if (halt) {
+    const char clock_lbl[] = "HALT   "; // Extra spaces to cover up "RUNNING"
+    DWORD dwBytesWritten;
+    WriteConsoleOutputCharacter(hConsole, clock_lbl, sizeof(clock_lbl) - 1, pos, &dwBytesWritten);
+  } else {
+    const char clock_lbl[] = "RUNNING";
+    DWORD dwBytesWritten;
+    WriteConsoleOutputCharacter(hConsole, clock_lbl, sizeof(clock_lbl) - 1, pos, &dwBytesWritten);
+  }
+}
+
+void updateALU(int subtract) {
+  if (subtract){
+    alu = register_A - register_B;
+  } else {
+    alu = register_A + register_B;
+  }
 }
 
 void updateDisplay() {
@@ -239,13 +292,81 @@ void updateDisplay() {
   printInstruction();
   printDecoder();
   printControl();
+  printClock();
+}
+
+decodeInstruction() {
+  unsigned char u_inst = ((register_I & 0xF0) >> 1) | (decoder_phase & 0x07);
+  decoder_output = u_instructions[u_inst];
+  // printRegister8(DECODER_X, DECODER_Y + 3, u_inst);
+}
+
+executeInstruction() {
+
+  // Deal with Halt first
+  if (decoder_output & HT) halt = 1;
+
+  // Subtract flag affects ALU output
+  updateALU(decoder_output & SU);
+
+  // Then we need to deal with outputs
+  // note - latest definition wins
+  if (decoder_output & RO) bus = RAM[memory_address & 0x0F];
+  if (decoder_output & IO) bus = register_I & 0x0F;
+  if (decoder_output & AO) bus = register_A;
+  if (decoder_output & EO) bus = alu;
+  if (decoder_output & CO) bus = program_counter;
+
+  // Now let's have some inputs
+  if (decoder_output & MI) memory_address = bus & 0x0F;
+  if (decoder_output & RI) RAM[memory_address & 0x0F] = bus;
+  if (decoder_output & II) {
+    register_I = bus;
+    decodeInstruction();
+  }
+  if (decoder_output & AI) {
+    register_A = bus;
+    updateALU(decoder_output & SU);
+  }
+  if (decoder_output & BI) {
+    register_B = bus;
+    updateALU(decoder_output & SU);
+  }
+  if (decoder_output & OI) register_O = bus;
+  if (decoder_output & JP) program_counter = bus & 0x0F;
+
+  // Finally do we increment the program counter?
+  if (decoder_output & CE) program_counter++;
+}
+
+step() {
+  decodeInstruction();
+  executeInstruction();
+
+  updateDisplay();
+
+  decoder_phase = (decoder_phase + 1) % PHASE_COUNT;
+}
+
+reset() {
+  program_counter = 0;
+  decoder_phase = 0;
+  register_A = 0;
+  register_B = 0;
+  register_I = 0;
+  register_O = 0;
+  alu = register_A + register_B;
+  decoder_output = 0;
+
+  step();
 }
 
 void main() {
   // Create blank console
   hConsole =  CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
   SetConsoleActiveScreenBuffer(hConsole);
-  SetConsoleOutputCP(1252);
+  SetConsoleOutputCP(1252); // Windows-1252 ANSI Latin 1
+  // SetConsoleOutputCP(65001);  // UTF-8
 
   // Move Cursor out of the way and hide
   {
@@ -260,19 +381,22 @@ void main() {
 
   printLabels();
 
-  register_B = 255;
+  step();
 
   while(1){
-    updateDisplay();
 
-    program_counter++;
-    register_A++;
-    register_B--;
-    updateALU();
-    memory_address++;
-    decoder_phase++;
-
-    Sleep(100);
+    if(halt) {
+      if(_kbhit()) {
+        char c = _getch();
+        if(c == ' ') step();
+        else if(c == 'r') reset();
+        else if(c == 'a') halt = 0;
+      }
+      Sleep(100);
+    } else {
+      step();
+      Sleep(1000);
+    }
   }
 
 }
