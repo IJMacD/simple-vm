3494cc8f67958c065a12c76c4a595c8c32a844a3
Iain MacDonald <IJMacD@gmail.com>
2017-08-28 10:47:01 +0800

    Tweak project structure

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
 @echo off
+if not exist obj mkdir obj
+if not exist bin mkdir bin
+cd obj
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
 set compilerflags=/Od /Zi /EHsc
-set linkerflags=/OUT:vm.exe
-cl.exe %compilerflags% *.c windows/*.c /link %linkerflags%
+set linkerflags=/OUT:../bin/vm.exe
+cl.exe %compilerflags% ../src/*.c ../src/windows/*.c /link %linkerflags%
